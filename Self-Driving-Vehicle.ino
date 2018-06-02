ad4a45d06215636e077167218e64f70107a7f8b7
Sspyros <spyros.2@hotmail.com>
2018-06-02 16:53:33 +0300

    Final code and thesis

--- Self-Driving-Vehicle.ino
+++ Self-Driving-Vehicle.ino
@@ -11,7 +11,7 @@
 #include <PID_v1.h>
 #include <IRremote.h>
 #include "HC_SR04.h"
-
+#include "RunningMedian.h"
 
 // ================================================================
 // ===           	   	  	      PINS	            	            ===
@@ -51,20 +51,23 @@
 #define Kp 4.25
 #define Ki 0.005
 #define Kd 2.6
-#define PIDSampleTime 4
-#define midSpeed 125
-//#ifdef debug 1
+#define PIDSampleTime 10
+#define midSpeed 100
+#define distStop 30
+#define distAvoid 120
+#define distToGoalThresh 25
+#define debug 1
 
 //Initialize volatile variables (for Interrupt Service Routines - ISRs)
 volatile int Rcounter=0, Lcounter=0;
 int  buzzerTimer = 0, lastRTick=0, lastLTick=0, curMillis, state, last_state, printMillis = 0, headingError, duration, speedA, speedB, moving = 0, movingManual = 0, distToGoal;
-byte distToObs;
-unsigned long prevMillis, prevMillisH, prevMicrosL, rising_time, falling_time, pulse_length, startTime;
-float prevYpr=999,distance, distC = 0, distL = 0,distR = 0, phiG, phiC;
+byte distToObsRaw, distToObs, avoidFlag = 0, turnRight;
+unsigned long prevMillis, prevMillisH, prevMicrosL, rising_time, falling_time, pulse_length, startTime, turnMillis;
+float prevYpr=999,distance, distC = 0, distL = 0,distR = 0, phiG, phiC, phiCinR;
 double Setpoint = 0, Input, Output;
 
 //Initialize starting vehicle position and goal in cm
-float x = 0,		y = 0,		xG = 100,		yG = 100;
+float x = 0,		y = 0,		xG = -350,		yG = -150;
 
 //Initializations
 Servo leftServo;
@@ -74,6 +77,8 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 HC_SR04 us_sensor(trig, echo, echo_int);
 IRrecv irrecv(irRcvPin);
 decode_results results;
+RunningMedian usReadings = RunningMedian(15);
+RunningMedian InputMedian = RunningMedian(15);      
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
@@ -89,6 +94,10 @@ void setup() {
 	//Ultrasonic sensor
 	pinMode(trig, OUTPUT);
 	pinMode(echo, INPUT);
+
+ //LED 
+ pinMode(13, OUTPUT);
+ 
  //Motor pins
 	pinMode(enA, OUTPUT);
 	pinMode(in1, OUTPUT);
@@ -97,25 +106,27 @@ void setup() {
 	pinMode(in3, OUTPUT);
 	pinMode(in4, OUTPUT);
 
-  pinMode(buzzerPin, OUTPUT);
   digitalWrite(trig, LOW);
 
 	pinMode(rightEncPin,INPUT);
   pinMode(leftEncPin, INPUT);
 	digitalWrite(rightEncPin, HIGH);       // turn on pull-up resistor
 	digitalWrite(leftEncPin, HIGH);       // turn on pull-up resistor
-	
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
 	leftServo.attach(leftServoPin);
   leftServo.write(90);
   delay(1000);
-  //leftServo.detach();
+  leftServo.detach();
   
 	// Attach Interrupts
 	attachInterrupt(digitalPinToInterrupt(leftEncPin),Lcount,RISING);
 	attachInterrupt(digitalPinToInterrupt(rightEncPin),Rcount,RISING);
 	
 	myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(-125,125);
+  myPID.SetOutputLimits(-midSpeed, midSpeed);
   myPID.SetSampleTime(PIDSampleTime);
 
   us_sensor.begin();
@@ -124,7 +135,8 @@ void setup() {
   irrecv.enableIRIn();
   
 	//Temporary
-  phiG = readCompass() * 180/M_PI; 
+  //phiG = readCompass() * 180/M_PI; 
+  Serial.println("===================== READY =====================");
 }
 
 // ================================================================
@@ -135,31 +147,39 @@ void loop() {
   prevMillis = millis();
   
   if(us_sensor.isFinished()){
-    distToObs = us_sensor.getRange();
+    distToObsRaw = us_sensor.getRange();
+    //Simple running average
+//    distToObs = 0.9 * distToObsRaw + 0.1 * distToObs;
+//    Serial.print("Raw: ");
+//    Serial.print(distToObsRaw);
+//    Serial.print(" Avg: ");
+//    Serial.println(distToObs);
+    usReadings.add(distToObsRaw);
+//    Serial.println(usReadings.getMedian());
+    distToObs = usReadings.getMedian();
     us_sensor.start();
   }
   
-  phiC = readCompass();
+  phiCinR = readCompass();
+  phiC = phiCinR * 180/M_PI;
 
-  leftServo.write(90-(phiG - (phiC * 180/M_PI)));
-//  Serial.print("phiC: ");
-//  Serial.print(phiC * 180/M_PI);
-//  Serial.print(" phiG : ");
-//  Serial.println(phiG);
+  //Serial.println(subAngles(90, subAngles(phiG,phiC)));
+  leftServo.write(subAngles(90, subAngles(phiG,phiC)));
+  
   distR = 1.1 * (Rcounter - lastRTick); // 2 * pi * R / 20
   distL = 1.1 * (Lcounter - lastLTick);
   distC = (distR + distL)/2;
 
-  x = x + distC * sin(phiC);
-  y = y + distC * cos(phiC);
+  x = x + distC * sin(phiCinR);
+  y = y + distC * cos(phiCinR);
   
   lastRTick = Rcounter;
   lastLTick = Lcounter;
   
   //distC = 0.55 * (deltaR + deltaL);  //shortest calcualtion 
-  //phiG = (atan2((xG - x), (yG - y)) * 180 / M_PI);
-  if (phiG <180) phiG = ((int)phiG + 360) % 360; //typechange might be wrong
-  
+  phiG = (atan2((xG - x), (yG - y)) * 180 / M_PI);
+//  if (phiG <180) phiG = (phiG + 360) % 360; //typechange might be wrong
+  if ( phiG < 0 ) phiG = phiG + 360;
   distToGoal = sqrt(sq(xG - x) + sq(yG - y));
   
   if(irrecv.decode(&results)){
@@ -192,54 +212,103 @@ void loop() {
       stopAll();
       movingManual = 0;
       moving = 0;
+      leftServo.detach();
     }
     if (results.value == 3772793023) {
       moving = 1;
+      leftServo.attach(leftServoPin);
     }
     irrecv.resume();   
   }
 
-
-	if (distToObs < 15){
-		stopAll();
+  if ( distToGoal < distToGoalThresh && moving == 1 ){
     moving = 0;
-    //Serial.println("Obstacle in front");
-	}
-	else if (distToObs < 100){
-		//turn 45 right or left to avoid obs
-    Input = ((100-distToObs)*(phiG - (phiC * 180/M_PI)) + distToObs * ((phiC * 180/M_PI) - phiG  +- 90) / 100.0
-    myPID.Compute();    
-	}	
-	else{
-		//no obs ahead, go to goal
-		Input = phiG - (phiC * 180/M_PI);
-    myPID.Compute();
-	}
- 
-  if (moving == 1 && distToGoal > 20){
-    speedB = midSpeed + (int)Output;
-    speedA = midSpeed - (int)Output;
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    analogWrite(enA, adjustSpeed(speedA));
-    analogWrite(enB, adjustSpeed(speedB));
-    printStuff();
-  } else{
-    moving = 0;    
+    stopAll();
+    Serial.println("===================== ARRIVED =====================");
   }
   
+	if (distToObs < distStop){
+		stopAll();
+    moving = 0;
+    leftServo.detach();
+    digitalWrite(13, LOW);
+
+    if ( moving == 1){
+      Serial.println("===============Obstacle in front===========");
+    }
+	} else if (distToObs < distAvoid && distToObs > distStop){
+    
+		//turn 90deg right or left to avoid obs ( phiC - phiG + 90 )
+    InputMedian.add((distToObs*subAngles(phiG, phiC) + (distAvoid - distToObs)*subAngles(phiC, phiG + 90) )/distAvoid);
+    //Input = InputMedian.getAverage();
+    
+    if ( avoidFlag == 0 && y - yG < 0 ){
+      turnRight = 1;
+      avoidFlag = 1;
+      turnMillis = millis();
+    } else if (avoidFlag == 0 && y - yG > 0){
+      turnRight = 0;
+      avoidFlag = 1;
+      turnMillis = millis();
+    }
+    
+    if ( !turnRight ) Input = (distToObs*subAngles(phiG, phiC) + (100 - distToObs)*subAngles(phiC, phiG + 90) )/100; //left
+    if ( turnRight ) Input = (distToObs*subAngles(phiG, phiC) + (100 - distToObs)*subAngles(phiC, subAngles(phiG, 90)) )/100; //right
+    
+    //Input = phiG - (phiC * 180/M_PI);
+    myPID.Compute();
+    digitalWrite(13, HIGH);
+
+    if ( moving == 1){
+      if ( debug ) Serial.print("--");
+      speedB = midSpeed + (int)Output;
+      speedA = midSpeed - (int)Output;
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(enA, adjustSpeed(speedA));
+      analogWrite(enB, adjustSpeed(speedB));
+      if ( debug ) printStuff();
+   }  
+	}	else {
+		//no obs ahead, go to goal
+		//Input = phiG - (phiC * 180/M_PI);
+    InputMedian.add(subAngles (phiG, phiC));
+    Input = InputMedian.getAverage();
+    digitalWrite(13, LOW);
+
+    myPID.Compute();
+    
+    if ( moving == 1){
+      if ( debug ) Serial.print("++");
+      speedB = midSpeed + (int)Output;
+      speedA = midSpeed - (int)Output;
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(enA, adjustSpeed(speedA));
+      analogWrite(enB, adjustSpeed(speedB));
+      if ( debug ) printStuff();
+   }
+   if ( millis() - turnMillis > 1000) avoidFlag = 0;
+	}
+ 
   if (movingManual == 1){
     if (millis() - startTime < 100){
-      Serial.println("Moving manually...");
+      //Serial.println("Moving manually...");
+      //printStuff();
+      Serial.print(Rcounter);
+      Serial.print(" L: ");
+      Serial.println(Lcounter);
     }else{
       stopAll();
       movingManual = 0;
     }
 	}
  //Serial.println(millis() - prevMillis);
-  Serial.println(distToObs);
+
 }
 
 // ================================================================
@@ -284,6 +353,8 @@ void stopAll(){
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
 }
 
 void Rcount(){
@@ -304,6 +375,16 @@ int adjustSpeed(int speeed){
   } 
 }
 
+//difference between 2 angles in degrees
+float subAngles(float a, float b){
+  if ( a > 180 ) a -= 360;
+  if ( b > 180 ) b -= 360;
+  int ans = a - b;
+  if ( ans > 180 ) ans -= 360;
+  if ( ans < -180 ) ans += 360;
+  return ans;
+}
+
 //int readCompass(void)
 float readCompass(void)
 {
@@ -320,7 +401,7 @@ float readCompass(void)
 void printStuff(){
 	printMillis = millis();
 	Serial.print("Phi: ");
-  Serial.print(phiC* 180/M_PI);
+  Serial.print(phiC);
 	Serial.print(" -> ");
   Serial.print(phiG);
   Serial.print(" x: ");
@@ -331,18 +412,23 @@ void printStuff(){
   Serial.print(y);
   Serial.print(" -> ");
   Serial.print(yG);
-  Serial.print(" dist: ");
+  Serial.print(" distG: ");
   Serial.print(distToGoal);
-	//Serial.print(" Dist from goal: ");
-	//Serial.print(distToGoal);
-	//Serial.print("Input: ");
-	//Serial.print(Input);
+  Serial.print(" distO: ");
+  Serial.print(distToObs);
+  if ( turnRight ){
+    Serial.print(" Go Right ");
+  } else {
+    Serial.print(" Go Left ");
+  }
+	Serial.print(" Input: ");
+	Serial.print(Input);
+  Serial.print(" Out: ");
+  Serial.print(Output);
 	Serial.print(" Speed A: ");
 	Serial.print(adjustSpeed(speedA));
 	Serial.print(" B: ");
 	Serial.println(adjustSpeed(speedB));
-//	Serial.print(" PID out: ");
-//	Serial.println(Output);
 /*	Serial.print("Right:");
 	Serial.print(Rcounter); 
 	Serial.print("  ");
